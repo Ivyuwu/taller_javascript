c2a6e2e7562b0c9b2a4f9e6d1b724522dcd90adf
Ivyuwu <130815010+Ivyuwu@users.noreply.github.com>
2023-05-17 00:01:07 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -32,14 +32,11 @@ function updateDisplay(value) {
 }
 
 
-// Generate a random number between 1 and 100
 var randomNumber = Math.floor(Math.random() * 100) + 1;
 
 function checkGuess() {
-  // Get the user's guess
   var guess = document.getElementById('guess').value;
 
-  // Compare the guess with the random number
   if (guess == randomNumber) {
     document.getElementById('result').innerHTML = '¡Felicidades! ¡Has adivinado el número!';
   } else if (guess < randomNumber) {
@@ -51,7 +48,6 @@ function checkGuess() {
 
 
 function checkNumbers() {
-  // Get the user's numbers
   var number1 = document.getElementById('number1').value;
   var number2 = document.getElementById('number2').value;
   var number3 = document.getElementById('number3').value;
@@ -59,7 +55,6 @@ function checkNumbers() {
   var number5 = document.getElementById('number5').value;
   var number6 = document.getElementById('number6').value;
 
-  // Generate random winning numbers
   var winningNumbers = [];
   while (winningNumbers.length < 6) {
     var randomNum = Math.floor(Math.random() * 41) + 1;
@@ -68,15 +63,12 @@ function checkNumbers() {
     }
   }
 
-  // Convert user's numbers to an array
   var userNumbers = [parseInt(number1), parseInt(number2), parseInt(number3), parseInt(number4), parseInt(number5), parseInt(number6)];
 
-  // Check for matching numbers
   var matchedNumbers = userNumbers.filter(function(number) {
     return winningNumbers.includes(number);
   });
 
-  // Display the winning numbers and result
   var resultText = 'Los números sorteados son: ' + winningNumbers.join(', ');
   if (matchedNumbers.length === 6) {
     resultText += '<br>¡Felicidades! ¡Has ganado la lotería!';
@@ -93,12 +85,10 @@ function addTask() {
   var taskInput = document.getElementById('taskInput');
   var taskList = document.getElementById('taskList');
 
-  // Create a new list item
   var listItem = document.createElement('li');
   listItem.className = 'list-group-item d-flex justify-content-between align-items-center mb-2';
   listItem.innerHTML = taskInput.value;
 
-  // Create a button for completing the task
   var completeButton = document.createElement('button');
   completeButton.className = 'btn btn-success btn-sm';
   completeButton.innerHTML = 'Completado';
@@ -106,7 +96,6 @@ function addTask() {
     listItem.classList.toggle('completed');
   });
 
-  // Create a button for deleting the task
   var deleteButton = document.createElement('button');
   deleteButton.className = 'btn btn-danger btn-sm';
   deleteButton.innerHTML = 'Eliminar';
@@ -114,17 +103,14 @@ function addTask() {
     taskList.removeChild(listItem);
   });
 
-  // Append the buttons to the list item
   var buttonGroup = document.createElement('div');
   buttonGroup.className = 'btn-group';
   buttonGroup.appendChild(completeButton);
   buttonGroup.appendChild(deleteButton);
   listItem.appendChild(buttonGroup);
 
-  // Append the list item to the task list
   taskList.appendChild(listItem);
 
-  // Clear the input field
   taskInput.value = '';
 }
   
